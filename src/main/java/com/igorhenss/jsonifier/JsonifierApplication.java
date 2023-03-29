33f4b28beb58a2e587792404ac8f84c98754269b
Igor Silva <igorhenss@gmail.com>
2023-03-28 21:26:54 -0300

    #0001 - Kick off (:

---
+++ src/main/java/com/igorhenss/jsonifier/JsonifierApplication.java
@@ -0,0 +1,13 @@
+package com.igorhenss.jsonifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonifierApplication.class, args);
+	}
+
+}
